135a454f87b4e08471b3d143586584dcc733643e
Leon L. Robinson <leon.robinson@qa.com>
2022-02-09 10:25:37 +0000

    example to show a procedure

---
+++ lrnoprocedure.py
@@ -0,0 +1,19 @@
+def checkZ(z):
+    print(z)
+    if z < 5:
+        exit()
+
+
+
+x = 1000
+y = 2000
+z = x * y
+checkZ(z)
+z = y - x
+checkZ(z)
+z = y * (x/2)
+checkZ(z)
+z = x * x
+checkZ(z)
+z = y * y
+checkZ(z)
