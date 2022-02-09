162509d5175e655b8016337be2bcb650521db004
Leon L. Robinson <leon.robinson@qa.com>
2022-02-09 09:43:18 +0000

    procedure

---
+++ lrprocedure.py
@@ -0,0 +1,24 @@
+def piesprint():
+    print("I like pies")
+
+
+
+piesprint()
+
+
+piesprint()
+
+# I have written a procedure 
+# we call it like:
+# len(obj)
+# help()
+# type()
+# max()
+# min()
+# str()
+# int()
+# bool()
+piesprint()
+
+# float()
+# input()
